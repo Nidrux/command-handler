70724c51e60ddc59b721220c1c4e2511d9c7c65f
Nidrux <37250273+Nidrux@users.noreply.github.com>
2021-07-04 17:43:21 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,15 +1,14 @@
 module.exports = {
 	name: 'info',
 	description: 'Wow, so this is how you use embeds :o',
-    permission: 'ADMINISTRATOR',
-    onlyGuild: true,
-    channel: '861251403469291521',
+    	permission: 'ADMINISTRATOR',
+    	onlyGuild: true,
 	execute(client, message, args) {
 		const { MessageEmbed } = require('discord.js');
-        const { colors: { primary } } = require('../config.json');
-        let embed = new MessageEmbed()
-        .setColor(primary)
-        .setDescription(this.description);
-        message.reply(embed);
+	const { colors: { primary } } = require('../config.json');
+	let embed = new MessageEmbed()
+	.setColor(primary)
+	.setDescription(this.description);
+	message.reply(embed);
 	},
 };
